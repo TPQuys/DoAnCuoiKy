02183846fbcd3933177a0c05cfe04c32f9e671a9
TPQuys <quy097255@gmail.com>
2024-12-19 20:39:54 +0700

    fix railway url

--- fontend/src/utils/url.js
+++ fontend/src/utils/url.js
@@ -1,2 +1,3 @@
-export default 'https://doancuoiky-1.onrender.com/'
+// export default 'https://doancuoiky-1.onrender.com/'
 // export default 'http://localhost:8000/'
+export default 'https://doancuoiky-production.up.railway.app/'
